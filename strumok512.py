9865909d5952e53752b0273022617cc325a9be1f
Skakun Ostap <61621130+SkakunOstap@users.noreply.github.com>
2026-04-13 15:41:07 +0300

    Modified version of state dependencies

--- strumok512.py
+++ strumok512.py
@@ -9,13 +9,13 @@ def strumok512(T=11):
     eqs = '#%s %d Rounds\n' % (cipher_name, T)
     eqs += 'connection relations\n'
     for t in range(T):
-        # eqs += 'S_%d, S_%d, S_%d, S_%d\n' % (t + 16, t + 11, t + 2, t)
-        # eqs += 'S_%d, R_%d, R_%d, S_%d\n' % (t + 15, t + 1, t, t)
-        # eqs += 'R_%d, S_%d, R_%d\n' % (t + 2, t + 5, t)
-        eqs += 'S_%d, S_%d, S_%d, S_%d\n' % (t + 16, t + 13, t + 11, t)
-        eqs += 'S_%d, R2_%d, R2_%d, S_%d\n' % (t + 15, t + 1, t, t)
-        eqs += 'R2_%d, R2_%d, S_%d\n' % (t + 1, t, t + 13)
-        # eqs += 'R2_%d, R1_%d\n' % (t + 1, t)
+        # eqs += 'S_%d, S_%d, S_%d, S_%d\n' % (t + 16, t + 13, t + 11, t)
+        # eqs += 'S_%d, R2_%d, R2_%d, S_%d\n' % (t + 15, t + 1, t, t)
+        # eqs += 'R2_%d, R2_%d, S_%d\n' % (t + 1, t, t + 13)
+        eqs += 'S_%d, R1_%d, R2_%d => S_%d\n' % (t + 15, t, t, t)
+        eqs += 'S_%d, S_%d, S_%d => S_%d\n' % (t + 13, t + 11, t, t+16)
+        eqs += 'R1_%d => R2_%d\n' % (t, t+1)
+        eqs += 'S_%d, R2_%d => R1_%d\n' % (t+13, t, t+1)
     eqs += 'end'
     eqsfile_path = os.path.join(output_dir, 'relationfile_strumok.txt')
     with open(eqsfile_path, 'w') as relation_file:
